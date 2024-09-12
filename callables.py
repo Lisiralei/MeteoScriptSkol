7dbcb61329fcbc9b9e66c9796d0bba28eaeaf8ec
Lisiralei <realallerrr1@gmail.com>
2024-09-12 23:32:20 +1000

    Package for running functions as a CLI tool

---
+++ callables.py
@@ -0,0 +1,16 @@
+import asyncio
+import sys
+
+from script import meteo_data_collector
+
+
+def main():
+    args = sys.argv
+    # args[0] = current file
+    # args[1] = function name
+    # args[2:] = function args : (*unpacked)
+    asyncio.run(globals()[args[1]](*args[2:]))
+
+
+if __name__ == "__main__":
+    main()
